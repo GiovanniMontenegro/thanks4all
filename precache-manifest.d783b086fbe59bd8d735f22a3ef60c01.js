694144255d1d00ff2c4b103543e1c1cdbd96da56
giovanni.montenegroit@gmail.com <giovanni.montenegro@aurigaspa.com>
2019-12-12 15:54:13 +0100

    Updates

---
+++ precache-manifest.d783b086fbe59bd8d735f22a3ef60c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9e28725447748d8938d9e0a0653f7b",
+    "url": "/GiovanniMontenegro.github.io/thanks4all/index.html"
+  },
+  {
+    "revision": "96fb67a0f87ed508683e",
+    "url": "/GiovanniMontenegro.github.io/thanks4all/static/css/main.f7bd1c68.chunk.css"
+  },
+  {
+    "revision": "04d0bb2b60c43e0d9866",
+    "url": "/GiovanniMontenegro.github.io/thanks4all/static/js/2.3f2340d0.chunk.js"
+  },
+  {
+    "revision": "c30c8740d3e1cfa20f5f83b5f0c51ffc",
+    "url": "/GiovanniMontenegro.github.io/thanks4all/static/js/2.3f2340d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96fb67a0f87ed508683e",
+    "url": "/GiovanniMontenegro.github.io/thanks4all/static/js/main.1b6c6c52.chunk.js"
+  },
+  {
+    "revision": "1c3d25a2d8b81e4d6c39",
+    "url": "/GiovanniMontenegro.github.io/thanks4all/static/js/runtime-main.fd030844.js"
+  }
+]);
